67d9409f6c4597e8bb996e105aa7b6da9433c9c3
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-01-31 17:28:24 -0700

    Started project and added header to code

---
+++ BetterCalculatorProject/BetterCalculator.vb
@@ -0,0 +1,19 @@
+﻿Option Explicit On
+Option Strict On
+
+'Aftanom Anfilofieff
+'RCET0265
+'Spring 2021
+'Better Calculator
+'https://github.com/AftaAnfi/SimpleCalculator.git
+
+
+'DO THE LINK STILL
+
+Module BetterCalculator
+
+    Sub Main()
+
+    End Sub
+
+End Module
